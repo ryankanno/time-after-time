0e92a7f6f99274a492be629e77af41e629ff0d88
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-06-14 10:45:36 -0400

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
-from setuptools import find_packages
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
+import time_after_time
 
-setup(name='time-after-time',
-      version='0.0.1',
-      description='A tiny library to parse out various time grammars',
-      author='Ryan Kanno',
-      author_email='ryankanno@localkinegrinds.com',
-      packages=find_packages(),
-      url="https://github.com/ryankanno/time-after-time")
+packages = [
+    'time_after_time'
+]
+
+requires = ['pyparsing', 'python-dateutil', 'py-utilities==0.0.2']
+tests_require = ['flake8', 'mock', 'nose', 'nosexcover']
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('CHANGES') as f:
+    changes = f.read()
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+    'License :: OSI Approved :: MIT License',
+    'Topic :: Utilities'
+]
+
+setup(
+    name='time-after-time',
+    version=time_after_time.__version__,
+    description='A tiny library to parse out various time grammars',
+    long_description=readme + '\n\n' + changes,
+    author='Ryan Kanno',
+    author_email='ryankanno@localkinegrinds.com',
+    url="https://github.com/ryankanno/time-after-time",
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    package_dir={'time_after_time': 'time_after_time'},
+    install_requires=requires,
+    license='MIT',
+    tests_require=tests_require,
+    classifiers=classifiers,
+)
 
 # vim: filetype=python
